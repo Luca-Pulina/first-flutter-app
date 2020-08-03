9713400371d886a26e40697bd829b08da42abe36
lucapulina <lpulina@outlook.it>
2020-08-03 10:12:04 +0200

    first flutter app

---
+++ README.md
@@ -0,0 +1,8 @@
+# i_am_rich
+
+A new Flutter application.
+
+## Getting Started
+
+A really minimal example of Flutter app.
+
---
+++ android/app/src/main/kotlin/lucapulina/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package lucapulina.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
